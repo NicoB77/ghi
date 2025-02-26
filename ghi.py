cb18a31fe10bad4fcaa01add7b09a9f0549999ea
NicoB77 <Bourbaki77@outlook.com>
2025-02-26 18:01:00 +0100

    Monatsname korrigiert

--- ghi.py
+++ ghi.py
@@ -20,7 +20,7 @@ import openpyxl
 from requests_oauthlib import OAuth2Session
 
 
-MONTH_NAME_BY_NUMBER = {1: 'Januar', 2: 'Februar', 3: 'March', 4: 'April', 5: 'Mai', 6: 'Juni', 7: 'Juli', 8: 'August', 9: 'September', 10: 'Oktober', 11: 'November', 12: 'Dezember'}
+MONTH_NAME_BY_NUMBER = {1: 'Januar', 2: 'Februar', 3: 'März', 4: 'April', 5: 'Mai', 6: 'Juni', 7: 'Juli', 8: 'August', 9: 'September', 10: 'Oktober', 11: 'November', 12: 'Dezember'}
 MONTH_NUMBER_BY_LOWER_NAME = {v.lower(): k for k, v in MONTH_NAME_BY_NUMBER.items()}
 DAY_SHIFT_START = datetime.time(10)
 NIGHT_SHIFT_START = datetime.time(20)
